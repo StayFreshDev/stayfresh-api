98df5f27a3971a4d9dff210e1bc0d826ad3becb7
Antoineg3802 <antoine.guerin@cyclable.com>
2023-07-11 15:35:40 +0200

    feat: remove jade package

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ var cors = require('cors')
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json());
